b0d9ae7c6bc97bea1104f07260d149cabce806dc
Ian Lavapiez <ianhero.lavapiez@nutricoach.pro>
2022-07-29 11:32:13 +0800

    feat: initial commit for contracts

---
+++ LandShiba.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+
+contract LandShiba is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 0.001 ether;
+    uint public MAX_SUPPLY = 3;
+
+    constructor() ERC721("Land Shiba", "LS") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.landshibafinance.app/tokens/";
+    }
+
+    function safeMint(address to) public payable {
+        require(totalSupply() < MAX_SUPPLY, "Can't mint more.");
+        require(msg.value >= mintRate, "Not enough BNB sent.");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function withdraw() public onlyOwner {
+        require(address(this).balance > 0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
+    }
+}
